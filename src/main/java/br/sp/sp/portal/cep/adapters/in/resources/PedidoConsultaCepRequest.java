020a380c47b39eb1d68f5ae3c7134e989f220bd7
Douglas <douglas.szlps@gmail.com>
2025-03-14 16:10:55 -0300

    features Douglas - task: fix via network entre servicos  WireMock com aplicacao no docker.

--- src/main/java/br/sp/sp/portal/cep/adapters/in/resources/PedidoConsultaCepRequest.java
+++ src/main/java/br/sp/sp/portal/cep/adapters/in/resources/PedidoConsultaCepRequest.java
@@ -1,5 +1,6 @@
 package br.sp.sp.portal.cep.adapters.in.resources;
 
+
 public record PedidoConsultaCepRequest(String cep,
                                        String exchangeConsulta) {
 }
--- src/main/java/br/sp/sp/portal/cep/infrastructure/configuration/WireMockConfig.java
+++
@@ -1,7 +0,0 @@
-package br.sp.sp.portal.cep.infrastructure.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WireMockConfig {
-}
